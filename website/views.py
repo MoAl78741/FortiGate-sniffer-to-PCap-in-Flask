823c92c9abb0b79fa13cb6a3814d8a4775451327
Mo Alves <6182432+MoAl78741@users.noreply.github.com>
2022-05-15 20:24:40 -0400

    js change for cursor wait when convert clicked

--- website/views.py
+++ website/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from . import db
-import json
 
 
 views = Blueprint('views', __name__)
